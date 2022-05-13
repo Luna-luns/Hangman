7ef9be4474b11ceb5495b95b198b44e01c2040a3
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-05-13 22:47:11 +0300

    Stage 4

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,23 +1,24 @@
 import random
 
 
-def greet_the_player() -> str:
+def greet_the_player(_word_with_hidden_letters: str) -> str:
     print(' '.join('HANGMAN'))
-    return input('Guess the word:').strip()
+    return input(f'Guess the word {_word_with_hidden_letters}:').strip()
 
 
-def choose_random_word(_list_of_words: list) -> str:
-    return random.choice(_list_of_words)
+def replace_chars(_random_word: str) -> str:
+    return _random_word[:3] + '-' * (len(_random_word) - 3)
 
 
-def is_right_word(_user_word: str, _random_word: str) -> str:
+def check_word(_user_word: str, _random_word: str) -> str:
     if _user_word == _random_word:
         return 'You survived!'
     return 'You lost!'
 
 
 list_of_words = ['python', 'java', 'swift', 'javascript']
-random_word = choose_random_word(list_of_words)
-user_word = greet_the_player()
-result = is_right_word(user_word, random_word)
+random_word = random.choice(list_of_words)
+word_with_hidden_letters = replace_chars(random_word)
+user_word = greet_the_player(word_with_hidden_letters)
+result = check_word(user_word, random_word)
 print(result)
