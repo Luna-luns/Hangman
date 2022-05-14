be544c2ea93425606f6870150aa7e85f755a7e69
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-05-14 20:10:19 +0300

    Stage 5

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,13 +1,18 @@
 import random
 
 
-def greet_the_player(_word_with_hidden_letters: str) -> str:
-    print(' '.join('HANGMAN'))
-    return input(f'Guess the word {_word_with_hidden_letters}:').strip()
+def open_char(_word_with_hidden_letters: str, _random_word: str, _user_input: str) -> str:
+    result = ''
+    for i in range(len(_random_word)):
+        if _random_word[i] == _user_input:
+            result += _random_word[i]
+        else:
+            result += _word_with_hidden_letters[i]
+    return result
 
 
 def replace_chars(_random_word: str) -> str:
-    return _random_word[:3] + '-' * (len(_random_word) - 3)
+    return _random_word[:0] + '-' * len(_random_word)
 
 
 def check_word(_user_word: str, _random_word: str) -> str:
@@ -18,7 +23,14 @@ def check_word(_user_word: str, _random_word: str) -> str:
 
 list_of_words = ['python', 'java', 'swift', 'javascript']
 random_word = random.choice(list_of_words)
+random_word_set = set(random_word)
 word_with_hidden_letters = replace_chars(random_word)
-user_word = greet_the_player(word_with_hidden_letters)
-result = check_word(user_word, random_word)
-print(result)
+print(' '.join('HANGMAN'), '\n')
+for _ in range(8):
+    print(word_with_hidden_letters)
+    user_input = input('Input a letter:').strip()
+    if user_input in random_word_set:
+        word_with_hidden_letters = open_char(word_with_hidden_letters, random_word, user_input)
+    else:
+        print("That letter doesn't appear in the word.")
+print('\n' + 'Thanks for playing!')
