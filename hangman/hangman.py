8a4b02d52d47d0b6f19d54a9c98659d5d73e978c
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-05-15 15:42:05 +0300

    Final stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -19,7 +19,7 @@ def replace_chars(_random_word: str) -> str:
 def check_success(_user_word: str, _random_word: str) -> str:
     if _user_word == _random_word:
         return f'\nYou guessed the word {_user_word}!\nYou survived!'
-    return '\nYou lost!'
+    return 'You lost!'
 
 
 def check_correct_input(_user_input: str, _english_alphabet: str, _chars_set: set) -> str:
@@ -31,28 +31,47 @@ def check_correct_input(_user_input: str, _english_alphabet: str, _chars_set: se
         return "You've already guessed this letter."
 
 
-list_of_words = ['python', 'java', 'swift', 'javascript']
-english_alphabet = string.ascii_lowercase
-random_word = random.choice(list_of_words)
-random_word_set = set(random_word)
-word_with_hidden_letters = replace_chars(random_word)
-chars_set = set()
+def choose_action() -> str:
+    return input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:').strip()
+
+
 print(' '.join('HANGMAN'), '\n')
-attempts = 8
-while attempts > 0:
-    print(word_with_hidden_letters)
-    user_input = input('Input a letter:').strip()
-    check_result = check_correct_input(user_input, english_alphabet, chars_set)
-    if check_result:
-        print(check_result)
-    else:
-        if user_input in random_word_set:
-            word_with_hidden_letters = open_char(word_with_hidden_letters, random_word, user_input)
-            chars_set.add(user_input)
+times_win = 0
+times_loss = 0
+while True:
+    action = choose_action()
+    if action == 'play':
+        list_of_words = ['python', 'java', 'swift', 'javascript']
+        english_alphabet = string.ascii_lowercase
+        random_word = random.choice(list_of_words)
+        random_word_set = set(random_word)
+        word_with_hidden_letters = replace_chars(random_word)
+        chars_set = set()
+        attempts = 8
+        while attempts > 0:
+            print(word_with_hidden_letters)
+            user_input = input('Input a letter:').strip()
+            check_result = check_correct_input(user_input, english_alphabet, chars_set)
+            if check_result:
+                print(check_result)
+            else:
+                if user_input in random_word_set:
+                    word_with_hidden_letters = open_char(word_with_hidden_letters, random_word, user_input)
+                    chars_set.add(user_input)
+                else:
+                    print("That letter doesn't appear in the word.")
+                    chars_set.add(user_input)
+                    attempts -= 1
+            if word_with_hidden_letters == random_word:
+                break
+        check_win_loss = check_success(word_with_hidden_letters, random_word)
+        print(check_win_loss)
+        if check_win_loss == 'You lost!':
+            times_loss += 1
         else:
-            print("That letter doesn't appear in the word.")
-            chars_set.add(user_input)
-            attempts -= 1
-    if word_with_hidden_letters == random_word:
+            times_win += 1
+    if action == 'results':
+        print(f'You won: {times_win} times.\nYou lost: {times_loss} times.')
+    if action == 'exit':
         break
-print(check_success(word_with_hidden_letters, random_word))
+exit()
